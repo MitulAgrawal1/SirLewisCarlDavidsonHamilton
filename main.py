dae4bd8bdaa1caeb1d89e1c65a946fc55d88eeed
MitulAgrawal1 <141518530+MitulAgrawal1@users.noreply.github.com>
2023-08-07 22:53:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,48 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
-
-def normaluserlogin ():
-    user_id = input("\nPlease enter User_id: ")
-    password =  input("Please enter Password: ")
-    sql = "Select Password from user_id WHERE userid = %s"
-    values = (user_id,)
-    cursor.execute(sql, values)
+def login():
+    username = input("Enter the user_id of the user: ")
+    userpassword = input("Enter the user password: ")
+    sql = "SELECT * FROM user_id where userid = %s"
+    values = (username,)
+    cursor.execute(sql,values)
     rows = cursor.fetchall()
-    if rows[0][0] == password:
-        print("\n###################################################")
-        print("WELCOME BACK","user_id")
-        print("###################################################\n")
+    if rows[0][1] == username and rows[0][2] == userpassword and rows[0][3] == 1:
         normaluser()
+    elif rows[0][1] == username and rows[0][2] == userpassword and rows[0][3] == 2:
+        adminuser()
     else:
-        print("\nINCORRECT PASSWORD\n")
-        return
+        print("Error in login ...")
+
+def normaluser():
+    while True:
+        print("\n1.Search by Author")
+        print("2.Checkout a Book")
+        print("3.View Past Transactions")
+        print("4.Exit\n")
+        ch = int(input("Enter your choice:"))
+        if ch == 1:
+            searchbyauth()
+        if ch == 2:
+            checkout()
+            print("value deleted is ")
+        if ch == 3:
+            print("Good Bye")
+            break
+        if ch == 4:
+            print("Good Bye")
+            break
+    print("normaluser")  
+
+def searchbyauth():
+    authorname = input("\nEnter the author whose books you want to view: ")
+    sql = "SELECT * FROM book_id where Author = %s"
+    values = (authorname,)
+    cursor.execute(sql,values)
+    rows = cursor.fetchall()
+    for i in rows:
+        print("\nBook name =",i[0],"            Copies left =",i[3])
 
 def userformer():
     username = input("Enter the name of the user: ")
@@ -92,58 +118,15 @@ def allcheck():
     library_id = int(input("Please enter your 4 digit library number: "))
 
 
-def adminuserlogin(): 
-    return
-def normaluser():
-    while True:
-        print("1.Search by Author")
-        print("2.Checkout a Book")
-        print("3.View Past Transactions")
-        print("4.Exit")
-        ch = int(input("enter your choice"))
-        if ch == 1:
-            print("here")
-        if ch == 2:
-            checkout()
-            print("value deleted is ")
-        if ch == 3:
-            print("Good Bye")
-            break
-        if ch == 4:
-            print("Good Bye")
-            break
-    print("normaluser")  
+
 def adminuser():
     print("adminuser")  
-
-
-def login():
-    username = input("Enter the user_id of the user: ")
-    userpassword = input("Enter the user password: ")
-    sql = "SELECT * FROM user_id where userid = %s"
-    values = (username,)
-    cursor.execute(sql,values)
-    rows = cursor.fetchall()
-    if rows[0][1] == username and rows[0][2] == userpassword and rows[0][3] == 1:
-        normaluser()
-    elif rows[0][1] == username and rows[0][2] == userpassword and rows[0][3] == 2:
-        adminuser()
-    else:
-        print("Error in login ...")
 
 while True:
     print("###################################################")
     print("Library Login .....")
     print("###################################################\n")
-    print("1. Login as User")
-    print("2. Login as Admin\n")
-    role = int(input("Please enter your choice (1 or 2): "))
-    if role == 1:
-        normaluserlogin()
-    elif role == 2:
-        adminuserlogin()
-    else:
-        continue
+    login()
 
 
 
