b4c161f9a5a7d09cead0256216e4a1e5c0975afb
MitulAgrawal1 <141518530+MitulAgrawal1@users.noreply.github.com>
2023-10-24 22:55:36 +0530

    Update tableformer.py

--- tableformer.py
+++ tableformer.py
@@ -1,1 +1,22 @@
 
+import mysql.connector
+ 
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1234",
+    database="library"
+)
+ 
+cursor = mydb.cursor()
+
+sql = "Create table book_id (Name char(20), bookid char(20) Primary Key, Author char(20), Copies integer)"
+cursor.execute(sql) 
+mydb.commit()
+sql = "Create table user_id (Nameuser char(20), userid char(20) Primary Key, Password char(20), Role integer)"
+cursor.execute(sql)
+mydb.commit()
+sql = "Create table history (Name char(20), bookid char(20), userid char(20), check_out date, check_in date, due_date date)"
+cursor.execute(sql)
+mydb.commit()
+
